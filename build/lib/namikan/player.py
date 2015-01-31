034e75a3a69f677b3532dfefd77a1da2f3e15ceb
useanalias <use.an.alias@gmail.com>
2015-01-30 20:31:57 -0800

    Remove bugs from file import system

---
+++ build/lib/namikan/player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self):
+        self.items = []
+        self.namichoc = 0
+        self.namicoin = 0
+        self.namikans = self.generate_namikans()
+
+    def generate_namikans(self):
+        return [] 
+
+
